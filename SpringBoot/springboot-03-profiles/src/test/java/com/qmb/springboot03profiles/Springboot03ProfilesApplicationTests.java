ee1afeb9fdc78acc5e9588494a3b9f5d746c7c55
qimingbo <itmanito@163.com>
2026-04-13 21:53:07 +0800

    日期:2026/4/13 修改人:齐铭博 修改内容:使用profile功能,进行动态配置切换

---
+++ SpringBoot/springboot-03-profiles/src/test/java/com/qmb/springboot03profiles/Springboot03ProfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qmb.springboot03profiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot03ProfilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
