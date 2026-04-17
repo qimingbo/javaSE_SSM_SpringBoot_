e72f84c889581a6ad309c48d76b9c4b6f072b72e
qimingbo <itmanito@163.com>
2026-04-17 10:50:49 +0800

    日期:2026/4/17 修改人:齐铭博 修改内容:actuatot使用和配置

---
+++ SpringBoot/springboot-09-actuator/src/main/java/com/qmb/Springboot09ActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.qmb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot09ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot09ActuatorApplication.class, args);
+    }
+
+}
---
+++ SpringBoot/springboot-09-actuator/src/test/java/com/qmb/Springboot09ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qmb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot09ActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
