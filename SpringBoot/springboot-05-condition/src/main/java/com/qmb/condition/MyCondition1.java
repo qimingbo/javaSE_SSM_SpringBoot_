ba1b57b2955fcb264f7ec40a7100357947366937
qimingbo <itmanito@163.com>
2026-05-08 20:36:14 +0800

    日期：2026/5/8 修改人：齐铭博 修改内容:review代码

--- SpringBoot/springboot-05-condition/src/main/java/com/qmb/condition/MyCondition1.java
+++ SpringBoot/springboot-05-condition/src/main/java/com/qmb/condition/MyCondition1.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public class MyCondition1 implements Condition {
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-        // 需求2：导入注解属性值value指定坐标后常见bean
+        // 需求2：导入注解属性值value，指定坐标后创建bean
         // param1:context上下文对象。用于获取环境、IoC容器、ClassLoader对象
         // param2:metadata  当前被@Conditional判断的“目标对象”的注解元数据抽象
         // 获取注解属性值
